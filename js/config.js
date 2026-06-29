5986624c9d44e306969829cde8d0dc99c0fff536
mustaq911 <mustaq911@hotmail.com>
2026-06-29 09:55:05 -0400

    config.js

--- js/config.js
+++ js/config.js
@@ -1,12 +1,12 @@
 const CONFIG = {
   
   /* Production Configuration */
-  API_BASE_URL: "https://api.liquidationflips.ca"
-  const STRIPE_PUBLIC_KEY = CONFIG.STRIPE_PUBLIC_KEY || "pk_test_51SB3m1IIchWoEqKqXIfObOksOX98DDZm0LVyhIfO2zvEhrAa92QZWYUs6QdinWMW4uWRuCq7rmdlwOJy5Hk8EDXj006tmSDc3j";
+  API_BASE_URL: "https://api.liquidationflips.ca",
+  // const STRIPE_PUBLIC_KEY = CONFIG.STRIPE_PUBLIC_KEY || "pk_test_51SB3m1IIchWoEqKqXIfObOksOX98DDZm0LVyhIfO2zvEhrAa92QZWYUs6QdinWMW4uWRuCq7rmdlwOJy5Hk8EDXj006tmSDc3j";
   
   
   /* Local Configuration */
   //API_BASE_URL: "http://localhost:8080",
-  //STRIPE_PUBLIC_KEY: "pk_test_51SB3m1IIchWoEqKqXIfObOksOX98DDZm0LVyhIfO2zvEhrAa92QZWYUs6QdinWMW4uWRuCq7rmdlwOJy5Hk8EDXj006tmSDc3j",
+  STRIPE_PUBLIC_KEY: "pk_test_51SB3m1IIchWoEqKqXIfObOksOX98DDZm0LVyhIfO2zvEhrAa92QZWYUs6QdinWMW4uWRuCq7rmdlwOJy5Hk8EDXj006tmSDc3j",
   CANADA_POST_API_KEY: "AA11-AA11-AA11-AA11"
 };
